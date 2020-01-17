f6bdbea6ab3a9e5ed168a1899bdf4a617dc92553
William Blase <william.blase@gmail.com>
2020-01-17 03:28:22 -0800

    Add this project to portfolio (#1)

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Grid, Row, Col, Image } from "react-bootstrap";
+import { Grid, Row, Col, Button, Thumbnail } from "react-bootstrap";
 import "./Portfolio.css";
 
 export default class Portfolio extends Component {
@@ -9,14 +9,31 @@ export default class Portfolio extends Component {
         <Grid>
           <h2>Portfolio</h2>
           <Row>
-            <Col xs={12} sm={8} className="main-section">
-              <Grid>
-
-              </Grid>
+            <Col xs={8} sm={6} className="main-section">
+              <Thumbnail src="assets/app-1.png" alt="121x100">
+                <h3>Portfolio Site</h3>
+                <p>A React SPA using react-bootstap component library</p>
+                <p>
+                  <Button
+                    bsStyle="primary"
+                    href="https://jovial-einstein-dd01fa.netlify.com/"
+                  >
+                    Live Demo
+                  </Button>
+                  &nbsp;
+                  <Button
+                    bsStyle="default"
+                    href="https://github.com/williamdanger/wblase-portfolio"
+                  >
+                    Github Source
+                  </Button>
+                </p>
+              </Thumbnail>
             </Col>
             <Col xs={12} sm={4} className="sidebar-section">
               <p>
-                Here are a collection of exercises and side-projects. Click through to see them in action.
+                A collection of exercises and side-projects. Click through to
+                see the live version or source.
               </p>
             </Col>
           </Row>
